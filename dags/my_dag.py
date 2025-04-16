0ae965bf8c4eeace7e90abe85f8ce20ce2f92963
Arun Pillai <arundeepak89@gmail.com>
2025-04-16 15:21:32 -0400

    gx using dataframe only

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,22 +1,60 @@
-from airflow import DAG
+from airflow.decorators import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-from great_expectations_provider.operators.great_expectations import (
-    GreatExpectationsOperator,
-)
 from datetime import datetime
 
 
-with DAG(
+@dag(
     dag_id="my_dag",
     start_date = datetime(2025, 2, 1),
     schedule_interval="@once",
     catchup=False
-) as dag:
-    
+)
+def my_dag():
+
+    @task.virtualenv(
+        task_id="virtualenv_python", requirements=["great_expectations"], system_site_packages=False
+    )
+    def callable_virtualenv():
+        import great_expectations as gx
+        import pandas as pd
+
+        context=gx.get_context(mode="ephemeral")
+        data_source_name="my_dataframe"
+        data_source=context.data_sources.add_pandas(name = data_source_name)
+
+        data_asset_name="my_data_asset"
+        data_asset=data_source.add_dataframe_asset(name=data_asset_name)
+
+        batch_definition_name="my_batch_definition"
+        batch_definition=data_asset.add_batch_definition_whole_dataframe(batch_definition_name)
+
+        data_list=[
+            {'VendorID':1, 'Name':'Alex'},
+            {'VendorID':2, 'Name':'Junga'}
+        ]
+
+        dataframe=pd.DataFrame(data_list)
+
+        batch_parameters={"dataframe":dataframe}
+
+        batch_definition=(
+            context.data_sources.get(data_source_name)
+            .get_asset(data_asset_name)
+            .get_batch_definition(batch_definition_name)
+        )
+
+        expectation=gx.expectations.ExpectColumnValuesToNotBeNull(
+            column="VendorID"
+        )
+
+        batch=batch_definition.get_batch(batch_parameters=batch_parameters)
+        validation_results = batch.validate(expectation)
+        print(validation_results)
+
     drop_table = SQLExecuteQueryOperator(
         task_id="drop_table",
         conn_id="my_postgres",
-        sql="DROP TABLE IF EXISTS customer"
+        sql="sql/drop.sql"
     )
     
     create_table = SQLExecuteQueryOperator(
@@ -31,13 +69,7 @@ with DAG(
         sql="sql/load.sql"
     )
 
-    validate = GreatExpectationsOperator(
-        task_id = "gx_validate",
-        conn_id = 'my_postgres',
-        data_context_root_dir="include/gx",
-        data_asset_name="postgres.customer",
-        expectation_suite_name="strawberry_suite",
-        return_json_dict=True,
-    )
 
-    drop_table >> create_table >> load_table >> validate
+    drop_table >> create_table >> load_table >> callable_virtualenv()
+
+my_dag()
